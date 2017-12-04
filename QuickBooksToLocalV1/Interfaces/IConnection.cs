27c205d3c58a12911a70f7b50ae64148001051cf
David Francis <dandmitservices@gmail.com>
2017-12-04 18:39:28 +0000

    Added Iconnection

---
+++ QuickBooksToLocalV1/Interfaces/IConnection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickBooksToLocalV1.Interfaces
+{
+    public interface IConnection<T>
+    {
+        /// <summary>
+        /// Connect to Dataservice
+        /// IE   Database Web Ect
+        /// </summary>
+        /// <returns>session</returns>
+        T DbConnect();
+
+        /// <summary>
+        /// Closes Data Service
+        /// </summary>
+        /// <param name="obj">session</param>
+        /// <returns>True or False</returns>
+        bool DbClose(T obj);
+        
+    }
+}
